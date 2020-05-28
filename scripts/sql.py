49a48efc5d278dc33ac68cc49854300d5f51480e
Neel Radhakrishnan <neel@rdkr.uk>
2020-05-28 20:05:17 +0100

    add aws

--- scripts/sql.py
+++ scripts/sql.py
@@ -1,20 +1,20 @@
 import sys
 
 from sqlalchemy import create_engine
-from datetimerange import DateTimeRange
 
 sys.path.append(".")
-from scheduler.sql import PlayerDB, Base
+from scheduler import sql, dynamodb
 
-
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
 engine = create_engine("sqlite:///sqlalchemy_example.db")
+sql.Base.metadata.create_all(engine)
 
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
-Base.metadata.create_all(engine)
+db = sql.PlayerDB()
+dynamo = dynamodb.PlayerDB()
 
-# db = PlayerDB()
+for user, times in dynamo.get_players().items():
+    db.set_tz(user, dynamo.get_tz(user))
+    for time in times:
+        db.add_time(user, time)
+
 # db.add_time('nel', DateTimeRange("2020-05-22T10:00:00", "2020-05-22T15:00:00"))
 # db.get_players()
