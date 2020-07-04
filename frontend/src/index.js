4bf10a047c452bcabaa6888f90cd3ecf6e9e53d6
Neel Radhakrishnan <neel@rdkr.uk>
2020-07-04 11:45:00 +0100

    add v2 path and 404, add material css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,24 +8,48 @@ import * as serviceWorker from "./serviceWorker";
 import Login from "./components/Login.js";
 import App from "./components/App.js";
 
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import EventPage from "./components/EventPage";
+
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const NoMatch = () => {  return (<h1>page not found</h1>);};
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route exact path="/event">
-          <App />
-        </Route>
-        <Route path="/event/:event+">
-          <App />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <App />
+          </Route>
+          <Route exact path="/event">
+            <App />
+          </Route>
+          <Route path="/event/:event+">
+            <App />
+          </Route>
+          <Route exact path="/v2">
+          <EventPage />
+          </Route>
+          <Route path="/v2/:event+">
+            <EventPage />
+          </Route>
+          <Route component={NoMatch} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
